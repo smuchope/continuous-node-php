5b46255de9d80575d1eb0ce917f21a71ae77ccf1
root <root@DESKTOP-OE6P8DJ.localdomain>
2024-02-13 08:16:30 +0300

    init commit

---
+++ site/index.php
@@ -0,0 +1,15 @@
+<html>
+ <body>
+  <h1>Team Details:</h1>
+   <ul>
+    <?php
+    $json = file_get_contents('http://players/');
+    $players = json_decode($json)->players;
+    foreach ($players as $player) {
+      echo "<li>$player</li>";
+    }
+
+    ?>
+  </ul>
+</body>
+ </html>
